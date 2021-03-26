5a089d0f30556ddd6cd77706c3834dba05c93e68
Chad Dornfeld <cmdornfeld@gmail.com>
2021-03-26 11:01:00 -0500

    added codde for chartReducer.js

---
+++ src/redux/reducers/chartReducer.js
@@ -0,0 +1,10 @@
+const getChartInfo = ( state=[], action ) => {
+    switch( action.type ) {
+        case 'SET_CHART_INFO':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default getChartInfo;
