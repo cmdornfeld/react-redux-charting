b4e79c4ed400e9aa9c8f66150f533893bd2b3b02
Chad Dornfeld <cmdornfeld@gmail.com>
2021-03-28 16:18:13 -0500

    added 2nd function to chartSaga

--- src/redux/sagas/chartSaga.js
+++ src/redux/sagas/chartSaga.js
@@ -6,15 +6,31 @@ const apiKey = process.env.REACT_APP_API_KEY;
 function* getChartSaga(){
     try{
         const getResponse = yield axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MMM&apikey=${apiKey}`);
-        yield put({ type: 'SET_CHART_INFO', payload: getResponse.data})
+        yield put({ type: 'SET_CHART_INFO', payload: getResponse.data});
     }
-    catch(error){
+    catch( error ){
         console.log(`Error getting chart info, error = ${error}`);
+    }
+}
+
+function* searchNewTicker(action){
+    try{
+        const getResponse = yield axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${action.payload}&apikey=${apiKey}`);
+        if ( getResponse.data['Error Message'] ) {
+            alert(`Invalid ticker symbol.  Please try again.`);
+            getChartSaga();
+        } else {
+            yield put({type: 'SET_CHART_INFO', payload: getResponse.data});
+        }
+    }
+    catch( error ){
+        console.log(`Error searching new ticker, error = ${error}`);
     }
 }
 
 function* getChartInfo(){
     yield takeLatest('GET_CHART_INFO', getChartSaga);
+    yield takeLatest('SEARCH_NEW_TICKER', searchNewTicker);
 }
 
 export default getChartInfo;
