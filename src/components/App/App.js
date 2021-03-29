d19540b951308715b87c1bf0da20fe5c19ec8bd1
Chad Dornfeld <cmdornfeld@gmail.com>
2021-03-29 07:04:33 -0500

    added css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Chart from '../Chart/Chart';
 import Search from '../Search/Search';
+import './App.css';
 
 class App extends Component {
 
   render(){
     return (
-      <>
+      <div className="container">
         <div>
           <h1>Monthly Stock Prices</h1>
         </div>
@@ -16,7 +17,7 @@ class App extends Component {
         <div>
           <Chart />
         </div>
-      </>
+      </div>
     )
   }
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Search.css';
 
 class Search extends Component {
 
@@ -9,13 +10,13 @@ class Search extends Component {
 
     // function to update local state with which ticker symbol to search
     handleChange = (e) => {
-        console.log(e.target.value, this.state.searchTicker);
         this.setState({
             searchTicker: e.target.value.toUpperCase()
         })
     }
 
     // if false, dispatch action to update chart with new ticker symbol
+    // if true, alert user to try again
     handleSearch = () => {
         if ( this.state.searchTicker.length > 5 || this.state.searchTicker.length < 2) {
             alert(`Please enter valid ticker symbol. Try typing BRK-B for a well known company in Nebraska.`)
@@ -33,12 +34,13 @@ class Search extends Component {
                 <label for="ticker-symbol">
                     Enter Ticker Symbol
                 </label>
+                <br/>
                 <input
+                    className="ticker-input"
                     maxLength={5}
                     name="ticker-symbol"
                     onChange={this.handleChange}
                     placeholder="Try a company in Nebraska: BRK-B"
-                    size="40%"
                     type="text"
                     value={this.state.searchTicker} />
                 <button onClick={this.handleSearch}>Search</button>
